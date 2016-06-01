25099617d1f8f17b8e3ab46bdb77f0a8f726f73f
Antoni Martyniuk <a.martyniuk@convertiser.com>
2016-06-01 17:15:08 +0200

    adds failure behaviour demo

---
+++ myproject/apps/notes/tests/test_api.py
@@ -0,0 +1,17 @@
+from rest_framework.test import APITestCase
+
+
+class NotesAPITests(APITestCase):
+    """
+    Test Notes API
+    """
+    def test_will_fail(self):
+        """Test list"""
+        assert False
+
+    def test_2(self):
+        pass
+
+    def test_1(self):
+        pass
+
